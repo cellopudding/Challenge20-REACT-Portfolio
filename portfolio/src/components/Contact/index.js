b4b9f614f4b7049120479a2c9f9aefc318371e88
Lonnie Root <root.lonnie@gmail.com>
2023-04-26 15:35:03 -0400

    couple changes

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <>
+    <div id="contact-form">
+    <h1 class ="headings">CONTACT</h1>
+    <div class="contactwrapper">
+      
+
+    </div>
+    </div>
+    </>
   )
 }
