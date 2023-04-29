ffc37686d30321fa53c86c44a8132fb49ab5800b
Lonnie Root <root.lonnie@gmail.com>
2023-04-28 22:32:23 -0400

    more css

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./projects.css"
+import QuizardImage from "../../images/Quizard.png"
+import BreakingDadImage from "../../images/BreakingDad.png"
 
 export default function Projects() {
   return (
@@ -7,7 +9,14 @@ export default function Projects() {
     <div id="work">
       <h1 class="headings">PROJECTS</h1>
       <div class="gallery">
-
+        <div>
+        <a href="https://warm-shore-84336.herokuapp.com/" target="_blank" rel="noreferrer">
+        <img src={QuizardImage} alt="Quizard"></img>
+        </a>
+        <a href="https://cellopudding.github.io/BreakingDad/" target="_blank" rel="noreferrer">
+         <img src={BreakingDadImage} alt="BreakingDad"></img> 
+        </a>
+        </div>
 
      
 
