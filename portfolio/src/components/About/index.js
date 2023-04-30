5cf1ea5aa641d9455720fb6833fdbf7a70ab5a97
Lonnie Root <root.lonnie@gmail.com>
2023-04-30 14:31:02 -0400

    aboutmecss

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -5,17 +5,17 @@ export default function About() {
   return (
     <>
      <div class="about">
+      
         <h1 class ="headings">ABOUT ME</h1>
         <div class="aboutmewrapper">
-          
-          <img id="pic" src={AboutImage} alt="about me pic"/>
+        <img id="pic" src={AboutImage} alt="about me pic"/>
           </div>
         <div id="bio">
-            <h2>LONNIE ROOT</h2>
+            
             <p>Up until the Spring of 2020 I spent my professional career as a cellist with a wide-ranging background. One week I'd be performing with an orchestra
                 and the next I might be on the road with a national touring act. I enjoyed this creative blend and the constant challange of maintaining my craft. As a 
                 current student in the world of coding I find the same kind of exciting challenges. This page is meant to display a snapshot of my current projects and 
-                a way for me to exercise new skills. If you have questions, feedback, or want to give me a job please don't hesitate to reach out! </p>
+                a way for me to exercise new skills. If you have questions, feedback, or inquires, please reach out to me thorugh the contact page! </p>
         </div>
         </div>
         
